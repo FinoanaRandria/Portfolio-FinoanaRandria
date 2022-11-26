6c9345f35607024de895d99c8d5f095deb5cb044
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-11-26 20:16:42 +0300

    structuration

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import ""
+import "../../App.css"
 function Navbar() {
   return (
     <div>
         <header className='header' id="header">
             <nav className='nav container'>
-
+                <a href='#'className='nav_logo'>Finoana Ranri</a>
+                <div className='nav_menu' id="nav-menu">
+                    <ul className='nav__list grid'>
+                       <li className='nav_item'>
+                            <a href='#' className='nav_link'></a> 
+                       </li>
+                    </ul>
+                </div>
             </nav>
         </header>
 
