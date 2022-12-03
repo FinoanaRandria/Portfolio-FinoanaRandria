ad3d12995088fc0d194bcb769462578da9a48be5
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-03 21:39:19 +0300

    modification du navbar

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import icon4  from "../../assets/icon/home.png"
 import icon5  from "../../assets/icon/landscape.png"
 import icon6  from "../../assets/icon/send.png"
 import icon7  from "../../assets/icon/user.png"
-import icon8  from "../../assets/icon/times.svg"
+import icon8  from "../../assets/icon/tiames.svg"
 import icon9  from "../../assets/icon/applications.png"
 function Navbar() {
   return (
@@ -21,22 +21,22 @@ function Navbar() {
           <div className="nav__menu" id="nav-menu">
             <ul className="nav__list grid">
               <li className="nav__item">
-                <a href="#Home" className="nav__link"> Home<img src={icon4} width={25}/> </a>
+                <a href="#Home" className="nav__link"> <img src={icon4} width={25}/> Home </a>
               </li>
               <li className="nav__item">
-                <a href="#About" className="nav__link">About<img src={icon7} width={25}/></a>
+                <a href="#About" className="nav__link"><img src={icon7} width={25}/> About</a>
               </li>
               <li className="nav__item">
-                <a href="#Skills" className="nav__link">Skills<img src={icon2} width={25}/></a>
+                <a href="#Skills" className="nav__link"> <img src={icon2} width={25}/> Skills</a>
               </li>
               <li className="nav__item">
-                <a href="#Services" className="nav__link">Services<img src={icon1} width={25}/></a>
+                <a href="#Services" className="nav__link"> <img src={icon1} width={25}/> Services</a>
               </li>
               <li className="nav__item">
-                <a href="#Portfolio" className="nav__link">Portfolio<img src={icon5} width={25}/></a>
+                <a href="#Portfolio" className="nav__link"> <img src={icon5} width={25}/> Portfolio</a>
               </li>
               <li className="nav__item">
-                <a href="#ContactMe" className="nav__link">ContactME<img src={icon6} width={25}/></a>
+                <a href="#ContactMe" className="nav__link"> <img src={icon6} width={25}/> ContactME</a>
               </li>
             </ul>
                <img src={icon8} width={20} className="uil uil-times nav__close" id="nav-close"/>
