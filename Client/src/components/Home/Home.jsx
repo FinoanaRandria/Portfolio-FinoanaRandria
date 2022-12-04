1d12875d3e7f46792c3460ae9deeaf2551d7c253
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-04 21:23:56 +0300

    debut main

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -3,7 +3,18 @@ import "../../App.css"
 function Home() {
   return (
     <div>
-        
+        <main className='main'>
+             <section className='home section' id="home">
+                 <div className='home__container container grid'>
+                     <div className='home__content grid'>
+                          <div className='home__social'>
+                             <a href='' className='home_social-icon'></a>
+                          </div>
+                     </div>
+                 </div>   
+               
+             </section>
+        </main>
     </div>
   )
 }
