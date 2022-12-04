dd7e7ba111d5211e5ed5d904a4b3c43a8741b4e8
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-04 21:01:35 +0300

    menu mobile

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -22,12 +22,19 @@ function Navbar() {
         navMenu.classList.add('show-menu')
       })
     }
-     /* pour  hide */
+     /*😁 pour  hide */
     if(navClose){
       navClose.addEventListener('click',()=>{
         navMenu.classList.remove('show-menu')
       })
     }
+    /*😁 menu mobile   */
+      const navLink = document.querySelectorAll('.nav__link')
+        function linkAction(){
+          const navMenu = document.getElementById('nav-menu')
+            navMenu.classList.remove('show-menu')
+        }
+        navLink.forEach(n => n.addEventListener('click',linkAction))
   })
 
   return (
