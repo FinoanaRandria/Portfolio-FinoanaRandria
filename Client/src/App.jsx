c578eeb3b99d4d6b19c8d68b31044853bb989c9a
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-04 21:56:43 +0300

    main1

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar/Navbar'
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
        <Navbar/>
+       <Home/>
     </div>
   )
 }
--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import "../../App.css"
+import React from "react";
+import "../../App.css";
+import icon10 from "../../assets/icon/linkedin-in.svg"
+import icon11 from "../../assets/icon/github.svg"
+import icon12 from "../../assets/icon/facebook.svg"
 function Home() {
   return (
     <div>
-        <main className='main'>
-             <section className='home section' id="home">
-                 <div className='home__container container grid'>
-                     <div className='home__content grid'>
-                          <div className='home__social'>
-                             <a href='' className='home_social-icon'></a>
-                          </div>
-                     </div>
-                 </div>   
-               
-             </section>
-        </main>
+      <main className="main">
+        <section className="home section" id="home">
+          <div className="home__container container grid">
+            <div className="home__content grid">
+              <div className="home__social">
+                <a href="#"  target={'_blank'} className="home_social-icon">
+                     <img src={icon10} width={25}/>
+                </a>
+                <a href="#" target={'_blank'} className="home_social-icon">
+                      <img src={icon12} width={25}/>
+                </a>
+                <a href="#" target={'_blank'} className="home_social-icon">
+                      <img src={icon11} width={25}/>
+                </a>
+              </div>
+                <div className="home_img">
+                  
+                </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
