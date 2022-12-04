fbd80a26e1e28b5954f50778fdd3093a5c69bebe
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-04 20:51:30 +0300

    effet navbar

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useEffect} from "react";
 import "../../App.css";
 
 import icon1  from "../../assets/icon/briefcase.png"
@@ -11,6 +11,25 @@ import icon7  from "../../assets/icon/user.png"
 import icon8  from "../../assets/icon/tiames.svg"
 import icon9  from "../../assets/icon/applications.png"
 function Navbar() {
+   
+  useEffect(()=>{
+    const navMenu = document.getElementById('nav-menu')
+    const navToggle= document.getElementById('nav-toggle')
+    const navClose = document.getElementById('nav-close')
+
+    if(navToggle){
+      navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+      })
+    }
+     /* pour  hide */
+    if(navClose){
+      navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+      })
+    }
+  })
+
   return (
     <div>
       <header className="header" id="header">
@@ -21,22 +40,22 @@ function Navbar() {
           <div className="nav__menu" id="nav-menu">
             <ul className="nav__list grid">
               <li className="nav__item">
-                <a href="#Home" className="nav__link"> <img src={icon4} width={25}/> Home </a>
+                <a href="#Home" className="nav__link"> <img className="nav__icon" src={icon4} width={25}/> Home </a>
               </li>
               <li className="nav__item">
-                <a href="#About" className="nav__link"><img src={icon7} width={25}/> About</a>
+                <a href="#About" className="nav__link"><img className="nav__icon" src={icon7} width={25}/> About</a>
               </li>
               <li className="nav__item">
-                <a href="#Skills" className="nav__link"> <img src={icon2} width={25}/> Skills</a>
+                <a href="#Skills" className="nav__link"> <img className="nav__icon" src={icon2} width={25}/> Skills</a>
               </li>
               <li className="nav__item">
-                <a href="#Services" className="nav__link"> <img src={icon1} width={25}/> Services</a>
+                <a href="#Services" className="nav__link"> <img className="nav__icon" src={icon1} width={25}/> Services</a>
               </li>
               <li className="nav__item">
-                <a href="#Portfolio" className="nav__link"> <img src={icon5} width={25}/> Portfolio</a>
+                <a href="#Portfolio" className="nav__link"> <img className="nav__icon" src={icon5} width={25}/> Portfolio</a>
               </li>
               <li className="nav__item">
-                <a href="#ContactMe" className="nav__link"> <img src={icon6} width={25}/> ContactME</a>
+                <a href="#ContactMe" className="nav__link"> <img className="nav__icon" src={icon6} width={25}/> ContactME</a>
               </li>
             </ul>
                <img src={icon8} width={20} className="uil uil-times nav__close" id="nav-close"/>
