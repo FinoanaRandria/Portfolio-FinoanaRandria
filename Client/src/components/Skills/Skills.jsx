64e7b9ce00d96d44c17bda7bde4230301ab3fbe7
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-07 19:10:33 +0300

    skill3

--- Client/src/components/Skills/Skills.jsx
+++ Client/src/components/Skills/Skills.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import "../../App.css";
 import bracket from "../../assets/svg/bracket.svg";
 import angle from "../../assets/svg/angle-down.svg";
+import server from "../../assets/svg/server.svg";
+import swatch from "../../assets/svg/swatchbook.svg"
 function Skills() {
   return (
     <div>
-       {/* skills part 1 */}
+     
       <section className="skills section">
         <h2 className="section__title">Skills</h2>
         <span className="section__subtitle">My technical level</span>
 
         <div className="skills__container container grid">
           <div>
+                  {/* skills part 1 */}
             <div className="skills__content">
               <div className="skills__header">
                 <img className="skills__icon" src={bracket} width={27} />
@@ -65,7 +68,117 @@ function Skills() {
                 </div>
               </div>
             </div>
+                   {/* skills part 2 */} 
+                   <div className="skills__content">
+              <div className="skills__header">
+              <img className="skills__icon" src={server} width={18} />
+                <div>
+                  <h1 className="skills__title">Backend developer</h1>
+                  <span className="skills__subtitle">
+                    plus de 2 ans d'experiences
+                  </span>
+                </div>
+                <img className="skills__arrow" src={angle} width={15} />
+              </div>
+              <div className="skills__list grid">
+                <div className="skills__data">
+                  <div className="skills__title">
+                    <h3 className="skills__name">Node Js </h3>
+                    <span className="skills__number">60%</span>
+                  </div>
+                  <div className="skills__bar">
+                    <span className="skills__percentage skills__node"></span>
+                  </div>
+                </div>
+
+                <div className="skills__data">
+                  <div className="skills__title">
+                    <h3 className="skills__name">MongoDB</h3>
+                    <span className="skills__number">70%</span>
+                  </div>
+                  <div className="skills__bar">
+                    <span className="skills__percentage skills__mongo"></span>
+                  </div>
+                </div>
+
+                <div className="skills__data">
+                  <div className="skills__title">
+                    <h3 className="skills__name">SQL</h3>
+                    <span className="skills__number">53%</span>
+                  </div>
+                  <div className="skills__bar">
+                    <span className="skills__percentage skills__sql"></span>
+                  </div>
+                </div>
+
+                <div className="skills__data">
+                  <div className="skills__title">
+                    <h3 className="skills__name">Firebase</h3>
+                    <span className="skills__number">60%</span>
+                  </div>
+                  <div className="skills__bar">
+                    <span className="skills__percentage skills__firebase"></span>
+                  </div>
+                </div>
+              </div>
+            </div>       
           </div>
+                <div>
+                      {/* skills part 3 */} 
+                   <div className="skills__content">
+              <div className="skills__header">
+              <img className="skills__icon" src={swatch} width={22} />
+                <div>
+                  <h1 className="skills__title">Backend developer</h1>
+                  <span className="skills__subtitle">
+                    plus de 2 ans d'experiences
+                  </span>
+                </div>
+                <img className="skills__arrow" src={angle} width={15} />
+              </div>
+              <div className="skills__list grid">
+                <div className="skills__data">
+                  <div className="skills__title">
+                    <h3 className="skills__name">Node Js </h3>
+                    <span className="skills__number">60%</span>
+                  </div>
+                  <div className="skills__bar">
+                    <span className="skills__percentage skills__node"></span>
+                  </div>
+                </div>
+
+                <div className="skills__data">
+                  <div className="skills__title">
+                    <h3 className="skills__name">MongoDB</h3>
+                    <span className="skills__number">70%</span>
+                  </div>
+                  <div className="skills__bar">
+                    <span className="skills__percentage skills__mongo"></span>
+                  </div>
+                </div>
+
+                <div className="skills__data">
+                  <div className="skills__title">
+                    <h3 className="skills__name">SQL</h3>
+                    <span className="skills__number">53%</span>
+                  </div>
+                  <div className="skills__bar">
+                    <span className="skills__percentage skills__sql"></span>
+                  </div>
+                </div>
+
+                <div className="skills__data">
+                  <div className="skills__title">
+                    <h3 className="skills__name">Firebase</h3>
+                    <span className="skills__number">60%</span>
+                  </div>
+                  <div className="skills__bar">
+                    <span className="skills__percentage skills__firebase"></span>
+                  </div>
+                </div>
+              </div>
+            </div>  
+                </div>
         </div>
       </section>
     </div>
