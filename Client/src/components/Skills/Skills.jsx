e1265a512ef0345b7ff228e468240acd3c1fd48d
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-07 19:39:55 +0300

    bar

--- Client/src/components/Skills/Skills.jsx
+++ Client/src/components/Skills/Skills.jsx
@@ -3,18 +3,17 @@ import "../../App.css";
 import bracket from "../../assets/svg/bracket.svg";
 import angle from "../../assets/svg/angle-down.svg";
 import server from "../../assets/svg/server.svg";
-import swatch from "../../assets/svg/swatchbook.svg"
+import swatch from "../../assets/svg/swatchbook.svg";
 function Skills() {
   return (
     <div>
-     
       <section className="skills section">
         <h2 className="section__title">Skills</h2>
         <span className="section__subtitle">My technical level</span>
 
         <div className="skills__container container grid">
           <div>
-                  {/* skills part 1 */}
+            {/* skills part 1 */}
             <div className="skills__content">
               <div className="skills__header">
                 <img className="skills__icon" src={bracket} width={27} />
@@ -28,7 +27,7 @@ function Skills() {
               </div>
               <div className="skills__list grid">
                 <div className="skills__data">
-                  <div className="skills__title">
+                  <div className="skills__titles">
                     <h3 className="skills__name">HTML</h3>
                     <span className="skills__number">90%</span>
                   </div>
@@ -38,7 +37,7 @@ function Skills() {
                 </div>
 
                 <div className="skills__data">
-                  <div className="skills__title">
+                  <div className="skills__titles">
                     <h3 className="skills__name">CSS</h3>
                     <span className="skills__number">70%</span>
                   </div>
@@ -48,7 +47,7 @@ function Skills() {
                 </div>
 
                 <div className="skills__data">
-                  <div className="skills__title">
+                  <div className="skills__titles">
                     <h3 className="skills__name">Javascript</h3>
                     <span className="skills__number">53%</span>
                   </div>
@@ -58,7 +57,7 @@ function Skills() {
                 </div>
 
                 <div className="skills__data">
-                  <div className="skills__title">
+                  <div className="skills__titles">
                     <h3 className="skills__name">ReactJS</h3>
                     <span className="skills__number">60%</span>
                   </div>
@@ -68,10 +67,10 @@ function Skills() {
                 </div>
               </div>
             </div>
-                   {/* skills part 2 */} 
-                   <div className="skills__content">
+            {/* skills part 2 */}
+            <div className="skills__content">
               <div className="skills__header">
-              <img className="skills__icon" src={server} width={18} />
+                <img className="skills__icon" src={server} width={18} />
                 <div>
                   <h1 className="skills__title">Backend developer</h1>
                   <span className="skills__subtitle">
@@ -82,7 +81,7 @@ function Skills() {
               </div>
               <div className="skills__list grid">
                 <div className="skills__data">
-                  <div className="skills__title">
+                  <div className="skills__titles">
                     <h3 className="skills__name">Node Js </h3>
                     <span className="skills__number">60%</span>
                   </div>
@@ -92,7 +91,7 @@ function Skills() {
                 </div>
 
                 <div className="skills__data">
-                  <div className="skills__title">
+                  <div className="skills__titles">
                     <h3 className="skills__name">MongoDB</h3>
                     <span className="skills__number">70%</span>
                   </div>
@@ -102,7 +101,7 @@ function Skills() {
                 </div>
 
                 <div className="skills__data">
-                  <div className="skills__title">
+                  <div className="skills__titles">
                     <h3 className="skills__name">SQL</h3>
                     <span className="skills__number">53%</span>
                   </div>
@@ -112,7 +111,7 @@ function Skills() {
                 </div>
 
                 <div className="skills__data">
-                  <div className="skills__title">
+                  <div className="skills__titles">
                     <h3 className="skills__name">Firebase</h3>
                     <span className="skills__number">60%</span>
                   </div>
@@ -121,64 +120,54 @@ function Skills() {
                   </div>
                 </div>
               </div>
-            </div>       
+            </div>
           </div>
-                <div>
-                      {/* skills part 3 */} 
-                   <div className="skills__content">
+          <div>
+            {/* skills part 3 */}
+            <div className="skills__content">
               <div className="skills__header">
-              <img className="skills__icon" src={swatch} width={22} />
+                <img className="skills__icon" src={swatch} width={22} />
                 <div>
-                  <h1 className="skills__title">Backend developer</h1>
+                  <h1 className="skills__title">Designer</h1>
                   <span className="skills__subtitle">
-                    plus de 2 ans d'experiences
+                    plus de 1 ans d'experiences
                   </span>
                 </div>
                 <img className="skills__arrow" src={angle} width={15} />
               </div>
               <div className="skills__list grid">
                 <div className="skills__data">
-                  <div className="skills__title">
-                    <h3 className="skills__name">Node Js </h3>
-                    <span className="skills__number">60%</span>
+                  <div className="skills__titles">
+                    <h3 className="skills__name">Figma </h3>
+                    <span className="skills__number">49%</span>
                   </div>
                   <div className="skills__bar">
-                    <span className="skills__percentage skills__node"></span>
+                    <span className="skills__percentage skills__figma"></span>
                   </div>
                 </div>
 
                 <div className="skills__data">
-                  <div className="skills__title">
-                    <h3 className="skills__name">MongoDB</h3>
-                    <span className="skills__number">70%</span>
+                  <div className="skills__titles">
+                    <h3 className="skills__name">sketch</h3>
+                    <span className="skills__number">46%</span>
                   </div>
                   <div className="skills__bar">
-                    <span className="skills__percentage skills__mongo"></span>
+                    <span className="skills__percentage skills__sketch"></span>
                   </div>
                 </div>
 
                 <div className="skills__data">
-                  <div className="skills__title">
-                    <h3 className="skills__name">SQL</h3>
-                    <span className="skills__number">53%</span>
+                  <div className="skills__titles">
+                    <h3 className="skills__name">Photshop</h3>
+                    <span className="skills__number">63%</span>
                   </div>
                   <div className="skills__bar">
-                    <span className="skills__percentage skills__sql"></span>
-                  </div>
-                </div>
-
-                <div className="skills__data">
-                  <div className="skills__title">
-                    <h3 className="skills__name">Firebase</h3>
-                    <span className="skills__number">60%</span>
-                  </div>
-                  <div className="skills__bar">
-                    <span className="skills__percentage skills__firebase"></span>
+                    <span className="skills__percentage skills__photoshop"></span>
                   </div>
                 </div>
               </div>
-            </div>  
-                </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
