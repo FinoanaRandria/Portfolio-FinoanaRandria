590b4962044264fe8de831fa2fd33f73f1fc4c88
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-10 19:47:19 +0300

    qualif

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
+import Qualificaton from './components/Qualification/Qualificaton'
 import './App.css'
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
        <Home/>
       <About/>
       <Skills/>
+      <Qualificaton/>
     </div>
   )
 }
---
+++ Client/src/components/Qualification/Qualificaton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../App.css'
+function Qualificaton() {
+  return (
+    <div>
+        <section className='qualification section'>
+            <h2 className='section__title'>Qualification</h2>
+            <span className='section__subtitle'>My personal</span>
+        </section>
+    </div>
+  )
+}
+
+export default Qualificaton
