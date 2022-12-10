2097ed52c931a8926eff97694a9550a4d4a86662
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-10 19:53:50 +0300

    qualif1

--- Client/src/components/Qualification/Qualificaton.jsx
+++ Client/src/components/Qualification/Qualificaton.jsx
@@ -6,6 +6,12 @@ function Qualificaton() {
         <section className='qualification section'>
             <h2 className='section__title'>Qualification</h2>
             <span className='section__subtitle'>My personal</span>
+
+              <div className='qualification__container container'>
+                <div className='qualification__tabs'>
+                  <div className='qualification__button button--flex'></div>
+                </div>
+              </div>
         </section>
     </div>
   )
