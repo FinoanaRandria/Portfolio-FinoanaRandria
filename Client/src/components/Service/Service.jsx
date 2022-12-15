c4125b386ac00d254eb666cc3c681b51dceba49a
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-15 21:39:11 +0300

    service3

--- Client/src/components/Service/Service.jsx
+++ Client/src/components/Service/Service.jsx
@@ -3,7 +3,17 @@ import '../../App.css'
 function Service() {
   return (
     <div>
-      <section></section>
+      <section className='services section' id='services'>
+         <h2 className='section__title'>Services</h2>
+         <span className='section__subtitle'> Mes Services</span>
+
+           <div className='services__container container grid'>
+            {/* === service1=== */}
+              <div className='services__content'>
+                 <div> eto le logo kely </div>
+              </div>
+           </div>
+      </section>
 
     </div>
   )
