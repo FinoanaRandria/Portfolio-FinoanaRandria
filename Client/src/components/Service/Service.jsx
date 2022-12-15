0863b8f3614855ee8e74962a36d4d66097e74cbc
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-15 21:59:20 +0300

    service4

--- Client/src/components/Service/Service.jsx
+++ Client/src/components/Service/Service.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import '../../App.css'
+import React from "react";
+import "../../App.css";
+import icon16 from "../../assets/svg/uikit.svg";
+import icon17 from '../../assets/svg/arrow-right.svg'
 function Service() {
   return (
     <div>
-      <section className='services section' id='services'>
-         <h2 className='section__title'>Services</h2>
-         <span className='section__subtitle'> Mes Services</span>
+      <section className="services section" id="services">
+        <h2 className="section__title">Services</h2>
+        <span className="section__subtitle"> Mes Services</span>
 
-           <div className='services__container container grid'>
-            {/* === service1=== */}
-              <div className='services__content'>
-                 <div> eto le logo kely </div>
-              </div>
-           </div>
+        <div className="services__container container grid">
+          {/* === service1=== */}
+          <div className="services__content">
+            <div>
+                <img src={icon16} width={20}/> 
+                <h3>Ui/Ux <br/> Designer </h3>
+             </div>
+                <span className="button button--flex button--small button--link services__button">
+                    View More
+                    <img src={icon17} width={14}/> 
+                </span>
+          </div>
+        </div>
       </section>
-
     </div>
-  )
+  );
 }
 
-export default Service
+export default Service;
