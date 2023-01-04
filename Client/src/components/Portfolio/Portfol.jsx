af83b60b0881d80b42d5da9f1045db3b96c4c828
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2023-01-04 21:18:50 +0300

    portfolio1 assets update

--- Client/src/components/Portfolio/Portfol.jsx
+++ Client/src/components/Portfolio/Portfol.jsx
@@ -6,6 +6,32 @@ function Portfol() {
       <section className='portfolio section' id='portfolio'>
         <h2 className='section__title'>Portfolio</h2>
         <span className='section__subtitle'>Most recent Work</span>
+            {/* ==============portfolio 1 ================ */}
+ 
+        <div className='portfolio__container container'></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       </section>
     </div>
   )
