560fe683c061c64e4b0ee51632db488199a24f0f
FinoanaRandria <fitiavanarex4@gmail.com>
2023-03-08 12:30:09 +0300

    modif navbar

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
        <div className='flex justify-between items-center w-full
         h-full px-2 2xl:px-16'>
-           <Image src="/../public/assets/navLogo.png" alt='/' width='100' height='50' />
+           <Image src="/../public/assets/navLogo.png" alt='/' width='90' height='30' />
+           <div>
+              <ul className='hidden md:flex'>
+                 <Link href='/'>
+                     <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                 </Link>
+                 <Link href='/'>
+                     <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                 </Link>
+                 <Link href='/'>
+                     <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+                 </Link>
+                 <Link href='/'>
+                     <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+                 </Link>
+                 <Link href='/'>
+                     <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+                 </Link>
+              </ul>
+           </div>
        </div>
       
     </div>
