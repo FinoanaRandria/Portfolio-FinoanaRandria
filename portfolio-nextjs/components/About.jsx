91f57da6633e67cdd01bea389e52ff0508e5b92a
FinoanaRandria <fitiavanarex4@gmail.com>
2023-03-12 17:48:30 +0300

    fix navbar + about

--- portfolio-nextjs/components/About.jsx
+++ portfolio-nextjs/components/About.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
 const About = () => {
   return (
-    <div className='w-full md:h-screen flex items-center py-16'>
-       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
-         <div></div>
-         <div></div>
-       </div>
-    </div>
-  )
-}
+    <div className="w-full md:h-screen flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">About</p>
+          <h2 className="py-4">Who I Am</h2>
 
-export default About
+          <p className="py-2 text-gray-600">// I am not your normal developer</p>
+
+          <p className="py-2 text-gray-600">
+            I am a fast learner, passionate about solving complex problems and
+            and developing quality software. I also have an excellent ability to
+            work in a team and communicate effectively effectively with
+            stakeholders.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            My goal is to continue to gain knowledge and skills while working on
+            challenging projects that allow me to grow as a Fullstack developer.
+            I am confident that I will be an asset to any company looking for a
+            talented and motivated Fullstack developer.
+          </p>
+
+          <p className="py-2 text-gray-600 underline cursor-pointer"> Check out some of my latest projects.</p>
+        </div>
+        <div className="w-full h-full m-auto shadow-xl shadow-gray-400  rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+           < Image className="rounded-xl" src="/../public/assets/IMG_1171.JPG"  alt="pdp" width='225' height='50' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
        <div className='flex justify-between items-center w-full
-        h-full px-2 2xl:px-16'>
+        h-full px-2 2xl:px-16 bg-[#ecf0f3]'>
            <Image src="/../public/assets/navLogo.png" alt='/' width='125' height='50' />
            <div>
               <ul className='hidden md:flex'>
@@ -47,7 +47,7 @@ const Navbar = () => {
 
 
           <div className={nav ? ' md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
-             <div className={nav ? ' md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500': 'fixed left-[-100%] top-0  p-10 ease-in duration-500'}>
+             <div className={nav ? '  fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500': 'fixed left-[-100%] top-0  p-10 ease-in duration-500'}>
 
              <div>
                  <div className='flex w-full items-center justify-between'>
