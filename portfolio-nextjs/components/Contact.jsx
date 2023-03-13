dd6620708febc87e54914afc68030ddc40abccc5
FinoanaRandria <fitiavanarex4@gmail.com>
2023-03-13 20:07:20 +0300

    contact

--- portfolio-nextjs/components/Contact.jsx
+++ portfolio-nextjs/components/Contact.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
+import { HiOutlineChevronDoubleUp, HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
+import Link from "next/link";
 const Contact = () => {
   return (
     <div className="w-full lg:h-screen">
@@ -67,25 +68,51 @@ const Contact = () => {
                                    <form>
                                         <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                                                <div className="flex flex-col">
-                                                          <label className="upper" text-sm py-2>Name</label>
+                                                          <label className="uppercase text-sm py-2" text-sm py-2>Name</label>
                                                           <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" />
                                                </div>
 
 
                                                <div className="flex flex-col">
-                                                          <label className="upper" text-sm py-2>Phone Number</label>
+                                                          <label className="uppercase text-sm py-2" text-sm py-2>Phone Number</label>
                                                           <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" />
                                                </div>
                                         </div>
 
-                                        
+                                         <div className=" flex flex-col py-2">
+                                         <label className="uppercase text-sm py-2" text-sm py-2>Email</label>
+                                         <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" />
+                                         </div>
 
+                                         <div className=" flex flex-col py-2">
+                                         <label className="uppercase text-sm py-2" text-sm py-2>Subject</label>
+                                         <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" />
+                                         </div>
+
+
+                                         <div className=" flex flex-col py-2">
+                                         <label className="uppercase text-sm py-2" text-sm py-2>Message</label>
+                                         <textarea className="border-2 rounded-lg p-3 border-gray-300"  rows="10"></textarea>
+                                         </div>
+                                           <button className="w-full p-4 text-gray-100 mt-4">Send Message</button>
                                    </form>
 
                                </div>
              </div>
+                 
+
+
+
 
         </div>
+                <div className="flex justify-center py-12">
+                    <Link href='/'>
+                         <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 backdrop:">
+                           <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30}/>
+                         </div>
+                    </Link>
+                </div>
+          
       </div>
     </div>
   );
