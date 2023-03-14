2bd6cea984e8a0c56648339a3a0cf599f59aadfb
FinoanaRandria <fitiavanarex4@gmail.com>
2023-03-14 23:38:04 +0300

    contact mail fonctionnel

--- portfolio-nextjs/components/About.jsx
+++ portfolio-nextjs/components/About.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Image from "next/image";
+
 const About = () => {
   return (
     <div id="about" className="w-full md:h-screen flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
-          <p className="uppercase text-xl tracking-widest text-[#5651e5]">About</p>
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+            About
+          </p>
           <h2 className="py-4">Who I Am</h2>
 
-          <p className="py-2 text-gray-600">// I am Finoana Randri a junior developer</p>
+          <p className="py-2 text-gray-600">
+            // I am Finoana Randri a junior developer
+          </p>
 
           <p className="py-2 text-gray-600">
             I am a fast learner, passionate about solving complex problems and
@@ -24,13 +29,28 @@ const About = () => {
             talented and motivated Fullstack developer.
           </p>
 
-          <p className="py-2 text-gray-600 underline cursor-pointer"> Check out some of my latest projects.</p>
+          <p className="py-2 text-gray-600 underline cursor-pointer mb-5">
+            {" "}
+            Check out some of my latest projects.
+          </p>
 
-          <button className="w-[30%] p-4 text-gray-100 mt-4 mb-2 hover:scale-105 ease-in duration-300  ">CV</button>
-          
+          <div className="mb-7">
+          <a
+            target="_blank" href='https://gateway.pinata.cloud/ipfs/QmNT8ztJhjDJG5j5Co6F9WFxgv9Zmrh4pHL67Dg8h8KHjE?_gl=1*18cv2mk*_ga*NzkxMmIzOTItNzgyMC00Y2FmLWE5NWMtNWJhZTJlYjVmZTRk*_ga_5RMPXG14TE*MTY3ODgxNjk5MC4xLjEuMTY3ODgxNzE1NS40Mi4wLjA.'  download='cv.pdf'
+             className=" shadow-xl shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-[#5651e5] to-[#709dff] px-20 p-3 text-gray-100 transform hover:scale-125 "
+        >
+            CV
+          </a>
+          </div>
         </div>
         <div className="w-full h-full  m-auto shadow-xl shadow-gray-400  rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-           < Image className="rounded-xl" src="/../public/assets/IMG_1171.JPG"  alt="pdp" width='325' height='65' />
+          <Image
+            className="rounded-xl"
+            src="/../public/assets/IMG_1171.JPG"
+            alt="pdp"
+            width="325"
+            height="65"
+          />
         </div>
       </div>
     </div>
--- portfolio-nextjs/components/Contact.jsx
+++ portfolio-nextjs/components/Contact.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { HiOutlineChevronDoubleUp, HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Link from "next/link";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_rbxpfum",
+        "template_sm5ryp4",
+        form.current,
+        "ztS_JsitRrqmsJyXX"
+      )
+      .then(
+        (result) => {
+          alert('Email is send')
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
@@ -59,60 +83,82 @@ const Contact = () => {
           </div>
 
           {/* right */}
-  
-             <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
-                               
-                               <div className="p-4">
-                                  
 
-                                   <form>
-                                        <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-                                               <div className="flex flex-col">
-                                                          <label className="uppercase text-sm py-2" text-sm py-2>Name</label>
-                                                          <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" />
-                                               </div>
+          <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+            <div className="p-4">
+              {/* fromulaire  sisa */}
 
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2" text-sm py-2>
+                      Name
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                      name="user_name"
+                    />
+                  </div>
 
-                                               <div className="flex flex-col">
-                                                          <label className="uppercase text-sm py-2" text-sm py-2>Phone Number</label>
-                                                          <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" />
-                                               </div>
-                                        </div>
-
-                                         <div className=" flex flex-col py-2">
-                                         <label className="uppercase text-sm py-2" text-sm py-2>Email</label>
-                                         <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" />
-                                         </div>
-
-                                         <div className=" flex flex-col py-2">
-                                         <label className="uppercase text-sm py-2" text-sm py-2>Subject</label>
-                                         <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" />
-                                         </div>
-
-
-                                         <div className=" flex flex-col py-2">
-                                         <label className="uppercase text-sm py-2" text-sm py-2>Message</label>
-                                         <textarea className="border-2 rounded-lg p-3 border-gray-300"  rows="10"></textarea>
-                                         </div>
-                                           <button className="w-full p-4 text-gray-100 mt-4 hover:scale-105 ease-in duration-300 ">Send Message</button>
-                                   </form>
-
-                               </div>
-             </div>
-                 
-
-
-
-
-        </div>
-                <div className="flex justify-center py-12">
-                    <Link href='/'>
-                         <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 backdrop:">
-                           <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30}/>
-                         </div>
-                    </Link>
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2" text-sm py-2>
+                      Phone Number
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                      name="phone_number"
+                    />
+                  </div>
                 </div>
-          
+
+                <div className=" flex flex-col py-2">
+                  <label className="uppercase text-sm py-2" text-sm py-2>
+                    Email
+                  </label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="email"
+                    name="email"
+                  />
+                </div>
+
+                <div className=" flex flex-col py-2">
+                  <label className="uppercase text-sm py-2" text-sm py-2>
+                    Subject
+                  </label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                    name="subject"
+                  />
+                </div>
+
+                <div className=" flex flex-col py-2">
+                  <label className="uppercase text-sm py-2" text-sm py-2>
+                    Message
+                  </label>
+                  <textarea
+                    name="message"
+                    className="border-2 rounded-lg p-3 border-gray-300"
+                    rows="10"
+                  ></textarea>
+                </div>
+                <button  className="w-full p-4 text-gray-100 mt-4 hover:scale-105 ease-in duration-300 ">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center py-12">
+          <Link href="/">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 backdrop:">
+              <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30} />
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
