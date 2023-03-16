181d39352c6a421430c65d81fa7f834a755706f3
FinoanaRandria <fitiavanarex4@gmail.com>
2023-03-16 21:58:43 +0300

    fix2

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import pdp from '../assets/IMG_1171.JPG'
+import pdp from '../assets/imagepdp.JPG'
 import { motion } from "framer-motion";
 const About = () => {
   return (
