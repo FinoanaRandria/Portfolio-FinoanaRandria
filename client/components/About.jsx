d1eaffcef3b8f2d66a4bf466900179b8c28e65cf
FinoanaRandria <fitiavanarex4@gmail.com>
2023-03-16 21:51:34 +0300

    fix about

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import pdp from '../assets/IMG_1171.JPG'
 import { motion } from "framer-motion";
 const About = () => {
   return (
@@ -84,7 +85,7 @@ const About = () => {
         <div  className="w-full h-full  m-auto shadow-xl shadow-gray-400  rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
           <Image
             className="rounded-xl"
-            src="/../public/assets/IMG_1171.JPG"
+            src={pdp}
             alt="pdp"
             width="325"
             height="65"
