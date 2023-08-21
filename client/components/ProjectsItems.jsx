5d48c1e8e038b9de4415008c792a39b12eb849ad
FinoanaRandria <fitiavanarex4@gmail.com>
2023-08-21 09:53:03 +0300

    modification css button

--- client/components/ProjectsItems.jsx
+++ client/components/ProjectsItems.jsx
@@ -5,7 +5,7 @@ const ProjectsItems = ({title, backgroundImg,projectUrl,techno}) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff] ">
       <Image
-        className="rounded-xl group-hover:opacity-10"
+        className="rounded-xl group-hover:opacity-10 object-cover"
         src={backgroundImg}
         alt="/"
       />
@@ -15,7 +15,7 @@ const ProjectsItems = ({title, backgroundImg,projectUrl,techno}) => {
         </h3>
         <p className="pb-4 pt-2 text-white text-center">{techno}</p>
         <Link href={projectUrl}>
-          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
+          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer hover:bg-gradient-to-r from-[#5651e5] to-[#709dff] hover:text-white hover:duration-500">
             More Info
           </p>
         </Link>
