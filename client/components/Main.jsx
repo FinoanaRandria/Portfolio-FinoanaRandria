c482586e28c4a5ba25544031f2644cd952fd2bfc
FinoanaRandria <fitiavanarex4@gmail.com>
2023-08-23 12:48:53 +0300

    text modif2

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -22,7 +22,7 @@ const Main = () => {
             </span>{" "}
           </h1>
 
-          <h1 className="py-4 text-gray-700">A MERN-Stack Web Developer </h1>
+          <h1 className="py-4 text-gray-700">A Mern-Stack Web Developer </h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
             I am a Junior MERN-stack Developer with a passion for creating
             innovative and effective solutions to meet user needs. I am
